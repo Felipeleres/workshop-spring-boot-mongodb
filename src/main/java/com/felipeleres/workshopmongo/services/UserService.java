c3249be2711d84bb748cf59958cecb8b00854ed9
Felipe Leres <leresfelipe@gmail.com>
2025-03-08 18:06:04 -0300

    Insercao de usuario com POST

--- src/main/java/com/felipeleres/workshopmongo/services/UserService.java
+++ src/main/java/com/felipeleres/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipeleres.workshopmongo.domain.User;
+import com.felipeleres.workshopmongo.dto.UserDTO;
 import com.felipeleres.workshopmongo.repository.UserRepository;
 import com.felipeleres.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 		User user = repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não econtrado"));
 		return user;
 		}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+	}
 	
 	}
 
