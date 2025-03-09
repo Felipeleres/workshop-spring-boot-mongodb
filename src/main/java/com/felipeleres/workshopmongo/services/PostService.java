385441d4804427627612b6fdc104725973e24856
Felipe Leres <leresfelipe@gmail.com>
2025-03-08 23:40:52 -0300

    Obtendo um post por id

---
+++ src/main/java/com/felipeleres/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.felipeleres.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipeleres.workshopmongo.domain.Post;
+import com.felipeleres.workshopmongo.repository.PostRepository;
+import com.felipeleres.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Post user = repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não econtrado"));
+		return user;
+		}
+	
+
+	}
+
+
